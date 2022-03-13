16aab8971fe2bb687cd6b70d8b8f452bef034def
ASUS <ASUS@KYRELL-LAPTOP>
2022-03-13 23:27:06 +0900

    Add project files.

---
+++ W3-Penilaian/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace W3_Penilaian
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_text = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_proses = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input data: ";
+            // 
+            // lbl_text
+            // 
+            this.lbl_text.AutoSize = true;
+            this.lbl_text.Font = new System.Drawing.Font("Microsoft YaHei", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_text.Location = new System.Drawing.Point(10, 134);
+            this.lbl_text.Name = "lbl_text";
+            this.lbl_text.Size = new System.Drawing.Size(99, 27);
+            this.lbl_text.TabIndex = 1;
+            this.lbl_text.Text = "[EMPTY]";
+            this.lbl_text.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(388, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btn_proses
+            // 
+            this.btn_proses.Location = new System.Drawing.Point(15, 67);
+            this.btn_proses.Name = "btn_proses";
+            this.btn_proses.Size = new System.Drawing.Size(75, 23);
+            this.btn_proses.TabIndex = 3;
+            this.btn_proses.Text = "Proses";
+            this.btn_proses.UseVisualStyleBackColor = true;
+            this.btn_proses.Click += new System.EventHandler(this.btn_proses_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 181);
+            this.Controls.Add(this.btn_proses);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbl_text);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form Pengaturan Warna";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_text;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_proses;
+    }
+}
+
---
+++ W3-Penilaian/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W3_Penilaian
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        Color warnaDefault = Color.Black;
+        private void btn_proses_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text.Substring(0, 4).ToLower() == "isi:")
+                {
+                    lbl_text.Text = textBox1.Text.Substring(4);
+                }
+                else if (textBox1.Text.ToLower() == "hide")
+                {
+                    lbl_text.Visible = false;
+                }
+                else if (textBox1.Text.ToLower() == "shown")
+                {
+                    lbl_text.Visible = true;
+                }
+                else if (textBox1.Text.ToLower() == "besarkan")
+                {
+                    lbl_text.Font = new Font("Microsoft YaHei", lbl_text.Font.Size + 1, FontStyle.Bold);
+                }
+                else if (textBox1.Text.ToLower() == "kecilkan")
+                {
+                    lbl_text.Font = new Font("Microsoft YaHei", lbl_text.Font.Size - 1, FontStyle.Bold);
+                }
+                else if (textBox1.Text.ToLower() == "restart")
+                {
+                    lbl_text.ForeColor = warnaDefault;
+                    lbl_text.Visible = true;
+                    lbl_text.Font = new Font("Microsoft YaHei", 15, FontStyle.Bold);
+                    lbl_text.Text = "[EMPTY]";
+
+                }
+
+                else if (textBox1.Text.Substring(0, 6).ToLower() == "warna:")
+                {
+                    if (textBox1.Text.Substring(6).ToLower() == "biru")
+                    {
+                        lbl_text.ForeColor = Color.Blue;
+
+                    }
+                    else if (textBox1.Text.Substring(6).ToLower() == "merah")
+                    {
+                        lbl_text.ForeColor = Color.Red;
+
+                    }
+                    else if (textBox1.Text.Substring(6) == "default" || textBox1.Text.Substring(6) == "hitam")
+                    {
+                        lbl_text.ForeColor = warnaDefault;
+
+                    }
+                    else if (textBox1.Text.Substring(6).ToLower() == "hijau")
+                    {
+                        lbl_text.ForeColor = Color.Green;
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Syntax tidak terdaftar");
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Syntax tidak terdaftar");
+                }
+            }
+
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Syntax tidak terdaftar!");
+            }
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btn_proses.PerformClick();
+
+
+
+            }
+        }
+    }
+}
